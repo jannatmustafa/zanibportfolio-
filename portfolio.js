f478a99b300dc402b6e3eab3daf23cf536c09246
jannatmustafa <chaudharyjannat297@gmail.com>
2025-02-05 22:07:32 +0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text-jv", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
